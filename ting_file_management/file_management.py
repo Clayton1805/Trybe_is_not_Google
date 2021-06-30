b34d602dcc55d65e9e5d28a53458653181219e08
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-06-30 18:06:02 -0300

    req 2 passando local

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+
+    try:
+        with open(path_file) as file:
+            return [o.split("\n")[0] for o in file.readlines()]
+    except FileNotFoundError:
+        return sys.stderr.write(
+            "Arquivo statics/arquivo_nao_existe.txt não encontrado\n"
+        )

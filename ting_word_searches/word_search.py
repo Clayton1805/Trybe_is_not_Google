94af7ad1af4b4eeeb5817e1d08e0c626ae678256
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-07-01 00:06:39 -0300

    req 6 passando local retornando result

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    list_return = list()
+    for file_obj in instance.get_queue():
+        d = {
+            "palavra": word,
+            "arquivo": file_obj["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for index, line in enumerate(file_obj["linhas_do_arquivo"]):
+            number_line = index + 1
+            word_lower = word.lower()
+            line_lower = line.lower()
+            if word_lower in line_lower:
+                d["ocorrencias"].append({"linha": number_line})
+        if d["ocorrencias"]:
+            list_return.append(d)
+    return list_return
 
 
 def search_by_word(word, instance):
